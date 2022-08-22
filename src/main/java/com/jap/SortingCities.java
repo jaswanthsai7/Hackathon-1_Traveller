211c3ea1be8b46d8031e1d0ff039681ab1ad47b8
Jaswanth Sai <jasvanthsai7@gmail.com>
2022-08-22 10:54:13 +0530

    added the method to sort city names

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -1,15 +1,20 @@
 package com.jap;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
- *
  */
-public class SortingCities
-{
-    public static void main( String[] args )
-    {
+public class SortingCities {
+    public static void main(String[] args) {
         String[] cityNames = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
         int[] distanceInKms = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
     }
+
+    public String[] sortingCityNames(String[] cityNames) {
+        Arrays.sort(cityNames);
+        return cityNames;
+    }
+
 
 }
